088d9c2bf26bf18fa74b2488ce0478353d408533
Aurora-official <huanmeng0106@gmail.com>
2024-07-23 11:32:41 +0800

    init commit

---
+++ src/main/java/io/aurora/AuroraBoard.java
@@ -0,0 +1,17 @@
+package io.aurora;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AuroraBoard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
