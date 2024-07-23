d8549929ad96ab266a78afbc747b486f868f3cfe
Aurora-official <huanmeng0106@gmail.com>
2024-07-23 13:24:52 +0800

    修复: 修复配置文件不会自动创建以及服务器关闭数据丢失的问题

--- src/main/java/io/aurora/AuroraBoard.java
+++ src/main/java/io/aurora/AuroraBoard.java
@@ -1,27 +1,33 @@
 package io.aurora;
 
 import io.aurora.util.Color;
+import io.aurora.util.ConfigUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
-public final class AuroraBoard extends JavaPlugin implements Listener {
+public class AuroraBoard extends JavaPlugin implements Listener {
     private static final Logger LOGGER = Bukkit.getLogger();
 
     private Map<String, Integer> playerBlockCount = new HashMap<>();
     private ScoreboardManager manager;
     private Scoreboard board;
     private Objective objective;
+    private File dataFile;
+    private FileConfiguration dataConfig;
 
     @Override
     public void onLoad() {
@@ -32,17 +38,28 @@ public final class AuroraBoard extends JavaPlugin implements Listener {
     public void onEnable() {
         LOGGER.info(Color.YELLOW + "[Aurora] " + Color.RESET + "Plugin is enabled.");
         getServer().getPluginManager().registerEvents(this, this);
-        Plugin config = AuroraBoard.getPlugin(AuroraBoard.class);
+
+        ConfigUtil config = new ConfigUtil("config.yml");
 
         manager = Bukkit.getScoreboardManager();
         board = manager.getNewScoreboard();
         objective = board.registerNewObjective("blockCount", "dummy", Objects.requireNonNull(config.getConfig().getString("name")));
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        dataFile = new File(getDataFolder(), "data.yml");
+
+        if (!dataFile.exists()) {
+            dataFile.getParentFile().mkdirs();
+            saveResource("data.yml", false);
+        }
+
+        dataConfig = YamlConfiguration.loadConfiguration(dataFile);
+        loadBlockCounts();
     }
 
     @Override
     public void onDisable() {
         LOGGER.info(Color.YELLOW + "[Aurora] " + Color.RESET + "Plugin is disabled.");
+        saveBlockCounts();
     }
 
     @EventHandler
@@ -62,5 +79,25 @@ public final class AuroraBoard extends JavaPlugin implements Listener {
         Score score = objective.getScore(playerName);
         score.setScore(count);
         event.getPlayer().setScoreboard(board);
+    }
+
+    private void loadBlockCounts() {
+        for (String key : dataConfig.getKeys(false)) {
+            int count = dataConfig.getInt(key);
+            playerBlockCount.put(key, count);
+            Score score = objective.getScore(key);
+            score.setScore(count);
+        }
+    }
+
+    private void saveBlockCounts() {
+        for (Map.Entry<String, Integer> entry : playerBlockCount.entrySet()) {
+            dataConfig.set(entry.getKey(), entry.getValue());
+        }
+        try {
+            dataConfig.save(dataFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
