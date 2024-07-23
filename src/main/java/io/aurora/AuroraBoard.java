b2c8642aef8638660f31d9bc7a28ca04139e29a5
Aurora-official <huanmeng0106@gmail.com>
2024-07-23 11:41:44 +0800

    添加：添加插件加载提示

--- src/main/java/io/aurora/AuroraBoard.java
+++ src/main/java/io/aurora/AuroraBoard.java
@@ -1,17 +1,26 @@
 package io.aurora;
 
+import io.aurora.util.Color;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class AuroraBoard extends JavaPlugin {
+    private static final Logger LOGGER = Bukkit.getLogger();
+
+    @Override
+    public void onLoad() {
+        LOGGER.info(Color.YELLOW + "[Aurora] " + Color.RESET + "Plugin is loading.");
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        LOGGER.info(Color.YELLOW + "[Aurora] " + Color.RESET + "Plugin is enabled.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        LOGGER.info(Color.YELLOW + "[Aurora] " + Color.RESET + "Plugin is disabled.");
     }
 }
